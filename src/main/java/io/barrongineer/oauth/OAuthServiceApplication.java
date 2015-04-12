af92ca7bdd9127dbdbbf65e57035b957867c0682
Shaunn Barron <shaunnb@gmail.com>
2015-04-12 05:37:38 -0400

    added eureka service registration/discovery.

--- src/main/java/io/barrongineer/oauth/OAuthServiceApplication.java
+++ src/main/java/io/barrongineer/oauth/OAuthServiceApplication.java
@@ -2,11 +2,13 @@ package io.barrongineer.oauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@SpringBootApplication
+@EnableEurekaClient
 @EnableResourceServer
+@SpringBootApplication
 public class OAuthServiceApplication extends WebMvcConfigurerAdapter {
 
     public static void main(String[] args) {
